5d824f8c9f9ef41cc9ec30b9b80a61303efc9a2a
unknown <j_rozman@fanshaweonline.ca>
2018-01-10 14:17:21 -0500

    updated about

--- js/portfolio.js
+++ js/portfolio.js
@@ -37,5 +37,5 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].p;
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
